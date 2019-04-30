f75cfb6bb10660edad50a8f626ed73cd294e4cb4
Emily R <emilyroulund@gmail.com>
2019-04-30 20:24:03 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -29,7 +29,7 @@ def bulk_tweet_shortener (tweets)
   end
 end 
 
-def selective_tweet_shortener
+def selective_tweet_shortener(tweet)
   if tweet.length > 140 
     puts word_substituter(tweet)
   else 
