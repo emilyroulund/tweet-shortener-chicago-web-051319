adff3aa313a3c61144da7a9c7ae5051b92c2b7a6
Emily R <emilyroulund@gmail.com>
2019-04-30 20:15:07 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,3 +1,24 @@
-def dictionary (long_words, substitutes)
-  dictionary_hash = {long_words, subsitutes}
+def dictionary 
+  dictionary = {
+    "hello" => 'hi',
+    "to, two, too" => '2', 
+    "for, four" => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@", 
+    "and" => "&"
+    }
+end 
+
+def word_substituter (string)
+  string.each do 
+end 
+
+def bulk_tweet_shortener (tweets)
+end 
+
+def selective_tweet_shortener
+end 
+
+def shortened_tweet_truncator
 end 
