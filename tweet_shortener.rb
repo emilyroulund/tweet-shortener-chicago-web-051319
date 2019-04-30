57b964acce7bb22aee98b0c1c86b514132099b1c
Emily R <emilyroulund@gmail.com>
2019-04-30 20:29:09 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -39,6 +39,8 @@ end
 
 def shortened_tweet_truncator(tweet)
   if word_substituter(tweet).length > 140
-    tweet << "..."[138]
+    word_substituter(tweet)[0..136] + '...'
+  else
+    tweet
   end 
 end 
