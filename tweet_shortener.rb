683e00e3e0c17220585b343b1df33a8153bf0308
Emily R <emilyroulund@gmail.com>
2019-04-30 20:17:29 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -10,9 +10,15 @@ def dictionary
     }
 end 
 
-def word_substituter (string)
-  string.each do 
-end 
+def word_substituter (tweet)
+  tweet.split.collect do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
+    end
+  end.join(" ")
+end
 
 def bulk_tweet_shortener (tweets)
 end 
